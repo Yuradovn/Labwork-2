6288263005b6a92b1a298fb7b8b4e1da30e0479f
Yura <krulikyura228@gmail.com>
2019-03-15 22:07:32 +0200

    Project created
    Added files

---
+++ Project/functions.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "Header.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
---
+++ Project/main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include "Header.h"
+#include <fstream>
+
+using namespace std;
